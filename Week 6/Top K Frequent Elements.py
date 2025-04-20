5adff2ff2255805b6d0e854549fbb983c4c36d08
Andrew <113457160+batorgillts@users.noreply.github.com>
2025-04-19 21:38:46 -0400

    Create Top K Frequent Elements.py

---
+++ Week 6/Top K Frequent Elements.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        
+        if k == nums:
+            return nums
+        
+        heap = []
+        counter = {}
+        for n in nums:
+            counter[n] = 1 + counter.get(n, 0)
+        
+        for key, val in counter.items():
+            heapq.heappush(heap, (-val, key))
+        
+        res = []
+        while len(res) < k:
+            res.append(heapq.heappop(heap)[1])
+        
+        return res
